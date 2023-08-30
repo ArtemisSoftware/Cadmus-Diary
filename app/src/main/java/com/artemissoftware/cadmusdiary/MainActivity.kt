f5433fd3fc2799e9211be9b0471d761a42152fbc
gustavo <gustavo.isaac@gmail.com>
2023-08-30 04:56:19 +0100

    Splash screen

--- app/src/main/java/com/artemissoftware/cadmusdiary/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/cadmusdiary/MainActivity.kt
@@ -3,11 +3,15 @@ package com.artemissoftware.cadmusdiary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.artemissoftware.cadmusdiary.ui.theme.CadmusDiaryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContent {
             CadmusDiaryTheme {
             }
