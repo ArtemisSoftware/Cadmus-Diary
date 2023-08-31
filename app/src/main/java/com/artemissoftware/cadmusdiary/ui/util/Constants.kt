e80682753fa01d6a8538b846af1a33b6bc597eb9
gustavo <gustavo.isaac@gmail.com>
2023-08-31 04:49:07 +0100

    Constants

---
+++ app/src/main/java/com/artemissoftware/cadmusdiary/ui/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.cadmusdiary.ui.util
+
+object Constants {
+
+    const val APP_ID = "cadmus-diary-eaviu"
+    const val CLIENT_ID = "695925948481-3ukg3sag7pa8r6cdd04sd9d57vl7ieaj.apps.googleusercontent.com"
+}
