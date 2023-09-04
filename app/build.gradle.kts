16f6f56bf71e18b696a0317ceefa9751a73e208f
gustavo <gustavo.isaac@gmail.com>
2023-09-04 04:22:32 +0100

    Kotlin parcelize dependencie

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.com.android.application)
     alias(libs.plugins.org.jetbrains.kotlin.android)
     alias(libs.plugins.realm.kotlin)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
