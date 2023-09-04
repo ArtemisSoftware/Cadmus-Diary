0a2e3436ed187c2375af95f63051a25a913f1b7b
gustavo <gustavo.isaac@gmail.com>
2023-09-04 04:24:04 +0100

    UIEventsManager

---
+++ app/src/main/java/com/artemissoftware/cadmusdiary/domain/usecases/SignInWithMongoAtlasUseCase.kt
@@ -0,0 +1,10 @@
+package com.artemissoftware.cadmusdiary.domain.usecases
+
+class SignInWithMongoAtlasUseCase() {
+
+//    suspend operator fun invoke(tokenId: String) = withContext(Dispatchers.IO) {
+//        App.create(APP_ID).login(
+//            Credentials.google(tokenId, GoogleAuthType.ID_TOKEN),
+//        ).loggedIn
+//    }
+}
