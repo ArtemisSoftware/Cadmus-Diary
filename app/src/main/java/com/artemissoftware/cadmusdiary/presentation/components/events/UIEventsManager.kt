3db91661f683ccff870a23ff4e8cd5d9b3615fca
gustavo <gustavo.isaac@gmail.com>
2023-09-05 05:12:20 +0100

    AlertDialog

--- app/src/main/java/com/artemissoftware/cadmusdiary/presentation/components/events/UIEventsManager.kt
+++ app/src/main/java/com/artemissoftware/cadmusdiary/presentation/components/events/UIEventsManager.kt
@@ -1,4 +1,4 @@
-package com.artemissoftware.cadmusdiary.presentation.components
+package com.artemissoftware.cadmusdiary.presentation.components.events
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
