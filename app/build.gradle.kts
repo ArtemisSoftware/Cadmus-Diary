394dbd45311410c867a9a937cfdb29aad55d3ae8
gustavo <gustavo.isaac@gmail.com>
2023-09-06 05:21:46 +0100

    Coil dependencie

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,6 @@ dependencies {
     implementation(libs.one.tap.compose)
     implementation(libs.message.bar.compose)
     implementation(libs.realm.kotlin)
+
+    implementation(libs.coil.compose)
 }
