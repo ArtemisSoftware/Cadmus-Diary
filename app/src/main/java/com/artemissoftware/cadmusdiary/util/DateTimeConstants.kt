d877397a70a2013a573ae60d457757f2a83923e6
gustavo <gustavo.isaac@gmail.com>
2023-09-12 05:23:15 +0100

    DateTimeConstants

---
+++ app/src/main/java/com/artemissoftware/cadmusdiary/util/DateTimeConstants.kt
@@ -0,0 +1,8 @@
+package com.artemissoftware.cadmusdiary.util
+
+object DateTimeConstants {
+
+    const val FORMAT_dd_MM_yyyy = "dd MMM yyyy"
+    const val FORMAT_hh_mm_a = "hh:mm a"
+    const val FORMAT_dd_MMM_yyyy__hh_mm_a = "dd MMM yyyy, hh:mm a"
+}
