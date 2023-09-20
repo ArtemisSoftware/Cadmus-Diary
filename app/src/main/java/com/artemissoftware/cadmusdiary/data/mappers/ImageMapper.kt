082bf953a7b22e8cc9b1ee50be02a5b0fe2bab98
gustavo <gustavo.isaac@gmail.com>
2023-09-20 05:26:30 +0100

    ImageMapper

---
+++ app/src/main/java/com/artemissoftware/cadmusdiary/data/mappers/ImageMapper.kt
@@ -0,0 +1,13 @@
+package com.artemissoftware.cadmusdiary.data.mappers
+
+import android.net.Uri
+import com.artemissoftware.cadmusdiary.data.database.entity.ImageToUploadEntity
+import com.artemissoftware.cadmusdiary.domain.model.Picture
+
+fun Picture.toEntity(sessionUri: Uri): ImageToUploadEntity{
+    return ImageToUploadEntity(
+        remoteImagePath = remotePath,
+        imageUri = image.toString(),
+        sessionUri = sessionUri.toString()
+    )
+}
