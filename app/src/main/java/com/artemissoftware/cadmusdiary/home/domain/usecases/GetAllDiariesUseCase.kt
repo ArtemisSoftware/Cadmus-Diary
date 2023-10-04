bed9156e65f12c0b43d9aa5bf100199d08dda614
gustavo <gustavo.isaac@gmail.com>
2023-10-04 09:30:44 +0100

    Fix for empty list display

--- app/src/main/java/com/artemissoftware/cadmusdiary/home/domain/usecases/GetAllDiariesUseCase.kt
+++ app/src/main/java/com/artemissoftware/cadmusdiary/home/domain/usecases/GetAllDiariesUseCase.kt
@@ -1,8 +1,12 @@
 package com.artemissoftware.cadmusdiary.home.domain.usecases
 
 import com.artemissoftware.cadmusdiary.core.data.repository.MongoRepository
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.debounce
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.seconds
 
 class GetAllDiariesUseCase @Inject constructor(private val mongoRepository: MongoRepository) {
-    operator fun invoke() = mongoRepository.getAllDiaries()
+    @OptIn(FlowPreview::class)
+    operator fun invoke() = mongoRepository.getAllDiaries().debounce(2.seconds)
 }
