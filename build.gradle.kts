a5e2260c74259306726cfe12f0d36fcdd84399b4
gustavo <gustavo.isaac@gmail.com>
2023-10-06 19:16:01 +0100

    Core ui module

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,5 +8,6 @@ plugins {
     alias(libs.plugins.gms.googleServices) apply false
     alias(libs.plugins.com.google.devtools.ksp) apply false
     alias(libs.plugins.com.google.dagger.hilt.android) apply false
+    alias(libs.plugins.com.android.library) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,3 +16,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "CadmusDiary"
 include(":app")
+include(":core:ui")
