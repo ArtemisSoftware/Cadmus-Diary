b38e311d5a6b61f4acda2cf23600445277239369
gustavo <gustavo.isaac@gmail.com>
2023-10-06 19:09:49 +0100

    ProjectConfig

---
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -0,0 +1,6 @@
+object ProjectConfig {
+    const val compileSdk = 34
+    const val minSdk = 31
+    const val targetSdk = 33 // TODO: mudar para 34 quando atualizar todas as dependencias no fim
+    const val kotlinCompilerExtensionVersion = "1.4.6"
+}
